a5a5384dfe6670a63a0d80d2ad56ba4296f74d2a
Jas Der <jasmineder@gmail.com>
2016-01-25 17:12:17 +0000

    form actions added

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,17 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    artist: params[:artist],
+    author: params[:author]
+    )
+  @song.save
+  redirect '/songs'
+end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
